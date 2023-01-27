4396f0a3270a59c9e6a2d50e4fdb63c2726a0eef
Andre Betancourt <Andre.Betancourt@infobip.com>
2023-01-27 17:28:11 -0600

    Adding snackbar

--- tarea_1/lib/main.dart
+++ tarea_1/lib/main.dart
@@ -1,17 +1,32 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const SnackBarDemo());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mc Flutter Andre'),
+        ),
+        body: const SnackBarPage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class SnackBarPage extends StatefulWidget {
+  const SnackBarPage({super.key});
+
+  @override
+  State<SnackBarPage> createState() => _SnackBarPageState();
+}
+
+class _SnackBarPageState extends State<SnackBarPage> {
   bool tap1 = false;
   bool tap2 = false;
   bool tap3 = false;
@@ -19,89 +34,125 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Mc Flutter Andre'),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(5),
-          height: 140,
-          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(5),
+      height: 140,
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
+              Padding(
+                padding: EdgeInsets.all(1),
+                child: Icon(Icons.account_circle, size: 50),
+              ),
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(1),
-                    child: Icon(Icons.account_circle, size: 50),
+                  Text(
+                    "Flutter McFlutter",
+                    style: TextStyle(fontSize: 18),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Flutter McFlutter",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Text(
-                        "Experienced App Developer",
-                        style: TextStyle(fontSize: 11),
-                      )
-                    ],
+                  Text(
+                    "Experienced App Developer",
+                    style: TextStyle(fontSize: 11),
                   )
                 ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("123 Main Street", style: TextStyle(fontSize: 15)),
-                  Text("(123) 4567-8901", style: TextStyle(fontSize: 15))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                      iconSize: 30,
-                      color: tap1 ? Colors.indigo : Colors.black,
-                      onPressed: () {
-                        tap1 = !tap1;
-                        setState(() {});
-                      },
-                      icon: Icon(Icons.accessibility_new)),
-                  IconButton(
-                      iconSize: 30,
-                      color: tap2 ? Colors.indigo : Colors.black,
-                      onPressed: () {
-                        tap2 = !tap2;
-                        setState(() {});
-                      },
-                      icon: Icon(Icons.timer)),
-                  IconButton(
-                      iconSize: 30,
-                      color: tap3 ? Colors.indigo : Colors.black,
-                      onPressed: () {
-                        tap3 = !tap3;
-                        setState(() {});
-                      },
-                      icon: Icon(Icons.phone_android)),
-                  IconButton(
-                      iconSize: 30,
-                      color: tap4 ? Colors.indigo : Colors.black,
-                      onPressed: () {
-                        tap4 = !tap4;
-                        setState(() {});
-                      },
-                      icon: Icon(Icons.phone_iphone)),
-                ],
-              ),
+              )
             ],
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("123 Main Street", style: TextStyle(fontSize: 15)),
+              Text("(123) 4567-8901", style: TextStyle(fontSize: 15))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  iconSize: 30,
+                  color: tap1 ? Colors.indigo : Colors.black,
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Soy una persona!'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    tap1 = !tap1;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.accessibility_new)),
+              IconButton(
+                  iconSize: 30,
+                  color: tap2 ? Colors.indigo : Colors.black,
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Soy un cronómetro!'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    tap2 = !tap2;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.timer)),
+              IconButton(
+                  iconSize: 30,
+                  color: tap3 ? Colors.indigo : Colors.black,
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Soy un android!'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    tap3 = !tap3;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.phone_android)),
+              IconButton(
+                  iconSize: 30,
+                  color: tap4 ? Colors.indigo : Colors.black,
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Soy un iPhone!'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    tap4 = !tap4;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.phone_iphone)),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ tarea_1/lib/main2.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool tap1 = false;
+  bool tap2 = false;
+  bool tap3 = false;
+  bool tap4 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mc Flutter Andre'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(5),
+          height: 140,
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(1),
+                    child: Icon(Icons.account_circle, size: 50),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Flutter McFlutter",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        "Experienced App Developer",
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("123 Main Street", style: TextStyle(fontSize: 15)),
+                  Text("(123) 4567-8901", style: TextStyle(fontSize: 15))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                      iconSize: 30,
+                      color: tap1 ? Colors.indigo : Colors.black,
+                      onPressed: () {
+                        tap1 = !tap1;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.accessibility_new)),
+                  IconButton(
+                      iconSize: 30,
+                      color: tap2 ? Colors.indigo : Colors.black,
+                      onPressed: () {
+                        tap2 = !tap2;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.timer)),
+                  IconButton(
+                      iconSize: 30,
+                      color: tap3 ? Colors.indigo : Colors.black,
+                      onPressed: () {
+                        tap3 = !tap3;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.phone_android)),
+                  IconButton(
+                      iconSize: 30,
+                      color: tap4 ? Colors.indigo : Colors.black,
+                      onPressed: () {
+                        tap4 = !tap4;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.phone_iphone)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// void main() => runApp(SnackBarDemo());
+
+// class SnackBarDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'SnackBar Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('SnackBar Demo'),
+//         ),
+//         body: SnackBarPage(),
+//       ),
+//     );
+//   }
+// }
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Algo de código para ¡deshacer el cambio!
+              },
+            ),
+          );
+
+          // Encuentra el Scaffold en el árbol de widgets y ¡úsalo para mostrar un SnackBar!
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
+      ),
+    );
+  }
+}
